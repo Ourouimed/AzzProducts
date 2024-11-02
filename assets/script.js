06559aa2df09bf21ae541d75a49a94b05d8ab880
Ourouimed <166921147+Ourouimed@users.noreply.github.com>
2024-11-02 16:51:16 +0100

    c

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,145 @@
-let name ="Test"
+let products = [
+    {
+        title : "spotify account (12 mo)",
+        price : 100,
+        image : "./products/media/spotify.png",
+        discount : 0,
+        trend : true,
+        category : "Music",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
 
-export default name
+    },
+    {
+        title : "Netflix account (12 mo)",
+        price : 90,
+        image : "./products/media/netflix.png",
+        discount : 10,
+        trend : false,
+        category : "Movies & Tv",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "Canva Pro (unlimited)",
+        price : 130,
+        image : "./products/media/canva.png",
+        discount : 20,
+        trend : false,
+        category : "Design",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "EA FC25 Ultimate edition",
+        price : 200,
+        image : "./products/media/fc25.png",
+        discount : 50,
+        trend : true,
+        category : "Gaming",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "Efootball Coins - Mobile",
+        price : 20,
+        image : "./products/media/efootball.png",
+        discount : 15,
+        trend : true,
+        category : "Gaming",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "Red Dead Redemption II",
+        price : 300,
+        image : "./products/media/RDRII.png",
+        discount : 35,
+        trend : true,
+        category : "Gaming",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "xbox game pass (12mo)",
+        price : 100,
+        image : "./products/media/xbox.png",
+        discount : 5,
+        trend : false,
+        category : "Gaming",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "Shahid Vip (24mo)",
+        price : 100,
+        image : "./products/media/shahid.png",
+        discount : 30,
+        trend : false,
+        category : "Movies & Tv",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "Iptv Football (12mo)",
+        price : 80,
+        image : "./products/media/iptv.png",
+        discount : 0,
+        trend : false,
+        category : "Movies & Tv",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "Free Fire Account",
+        price : 50,
+        image : "./products/media/ff.png",
+        discount : 8.2,
+        trend : false,
+        category : "Gaming",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "TOD 12 mounth",
+        price : 300,
+        image : "./products/media/tod.jpg",
+        discount : 35,
+        trend : true,
+        category : "Movies & Tv",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "StarzPlay Premium 12mo",
+        price : 200,
+        image : "./products/media/SP.jpg",
+        discount : 15,
+        trend : false,
+        category : "Movies & Tv",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "StarzPlay Premium 12mo",
+        price : 200,
+        image : "./products/media/SP.jpg",
+        discount : 15,
+        trend : false,
+        category : "Movies & Tv",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+    {
+        title : "Adobe Photoshop",
+        price : 270,
+        image : "./products/media/ps.jpg",
+        discount : 70,
+        trend : false,
+        category : "Design",
+        description : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam omnis optio qui animi ullam eos praesentium esse ipsam et vero.',
+        link : "link here"
+    },
+]
+
+export default products
