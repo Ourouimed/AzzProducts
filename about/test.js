9395ee6e98d47c7a327f1d101af7a683a7994122
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2024-11-03 10:01:48 +0100

    j

--- about/test.js
+++ about/test.js
@@ -1,4 +1,3 @@
-let request = new XMLHttpRequest()
-request.open("GET" , "../products/products.json") 
-request.send
-console.log(request)
+fetch("../products/products.json").then(result =>{
+    console.log(result)
+})
