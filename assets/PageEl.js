5e1ac0fbcba9860bfc6c4dbf2cff7bd5522fe99a
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2024-11-03 13:56:12 +0100

    n

--- assets/PageEl.js
+++ assets/PageEl.js
@@ -1,92 +1,92 @@
 let header = document.querySelector("header")
 let nav =  document.querySelector("nav#top")
 let footer = document.querySelector("footer")
-let body = document.querySelectorAll("body")
-body.forEach(body => {
+let body = document.body;
+let Allbody = document.querySelectorAll("body")
+Allbody.forEach(body => {
   let src =  body.dataset.src
-
-nav.innerHTML =     `
-    <p class="notif-msg">black friday discounts : +50% discount for game products</p>
-      <ul class="social">
-        <li><a href="" class="instagram"></a></li>
-        <li><a href="" class="x-twitter"></a></li>
-        <li><a href="" class="facebook"></a></li>
-        <li><a href="" class="whatsapp"></a></li>
-      </ul>
-`
-
-header.innerHTML = `
-      <h3 class="logo">
-        <a href="${src}"> Social<span>Digital</span> </a>
-      </h3>
-      <ul class="navigation">
-        <li><a href="">Home</a></li>
-        <li><a href="">Privacy</a></li>
-        <li><a href="">About us</a></li>
-        <li><a href="">Privacy</a></li>
-        <li><a href="">Contact Us</a></li>
-      </ul>
-      <ul class="nav-btns">
-        <button class="dark-mode">
-          <i class="fa-solid fa-moon"></i>
-          <i class="fa-solid fa-sun"></i>
-        </button>
-        <a href="" class="becomeSeller">
-          All Products
-          <i class="fa-solid fa-store"></i>
-        </a>
-      </ul>
+  nav.innerHTML =     `
+      <p class="notif-msg">black friday discounts : +50% discount for game products</p>
+        <ul class="social">
+          <li><a href="" class="instagram"></a></li>
+          <li><a href="" class="x-twitter"></a></li>
+          <li><a href="" class="facebook"></a></li>
+          <li><a href="" class="whatsapp"></a></li>
+        </ul>
   `
 
+  header.innerHTML = `
+        <h3 class="logo">
+          <a href="${src}"> Social<span>Digital</span> </a>
+        </h3>
+        <ul class="navigation">
+          <li><a href="">Home</a></li>
+          <li><a href="">Privacy</a></li>
+          <li><a href="">About us</a></li>
+          <li><a href="">Privacy</a></li>
+          <li><a href="">Contact Us</a></li>
+        </ul>
+        <ul class="nav-btns">
+          <button class="dark-mode">
+            <i class="fa-solid fa-moon"></i>
+            <i class="fa-solid fa-sun"></i>
+          </button>
+          <a href="" class="becomeSeller">
+            All Products
+            <i class="fa-solid fa-store"></i>
+          </a>
+        </ul>
+    `
 
-footer.innerHTML = `
-    <div class="foot-top">
-        <div class="foot-div">
-          <h2>About Us</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sequi
-            error natus animi at ad perspiciatis, inventore saepe placeat
-          </p>
+
+  footer.innerHTML = `
+      <div class="foot-top">
+          <div class="foot-div">
+            <h2>About Us</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sequi
+              error natus animi at ad perspiciatis, inventore saepe placeat
+            </p>
+          </div>
+          <div class="foot-div">
+            <h2>Categories</h2>
+            <ul>
+              <li><a href="">Gaming</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">New</a></li>
+              <li><a href="">Entertiment</a></li>
+            </ul>
+          </div>
+          <div class="foot-div">
+            <h2>Categories</h2>
+            <ul>
+              <li><a href="">Gaming</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">New</a></li>
+              <li><a href="">Entertiment</a></li>
+            </ul>
+          </div>
+          <div class="foot-div">
+            <h2>Categories</h2>
+            <ul>
+              <li><a href="">Gaming</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">New</a></li>
+              <li><a href="">Entertiment</a></li>
+            </ul>
+          </div>
         </div>
-        <div class="foot-div">
-          <h2>Categories</h2>
-          <ul>
-            <li><a href="">Gaming</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">New</a></li>
-            <li><a href="">Entertiment</a></li>
-          </ul>
+        <div class="foot-bot">
+          <div class="copyrights">
+            All Rights Reserved <a href="#"> Digital Services </a
+            ><span class="date"></span> ©
+          </div>
+          <div class="powred-by">
+            Powered By
+            <a target="_blank" href="https://github.com/Ourouimed/">OurouiDev</a>
+          </div>
         </div>
-        <div class="foot-div">
-          <h2>Categories</h2>
-          <ul>
-            <li><a href="">Gaming</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">New</a></li>
-            <li><a href="">Entertiment</a></li>
-          </ul>
-        </div>
-        <div class="foot-div">
-          <h2>Categories</h2>
-          <ul>
-            <li><a href="">Gaming</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">New</a></li>
-            <li><a href="">Entertiment</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="foot-bot">
-        <div class="copyrights">
-          All Rights Reserved <a href="#"> Digital Services </a
-          ><span class="date"></span> ©
-        </div>
-        <div class="powred-by">
-          Powered By
-          <a target="_blank" href="https://github.com/Ourouimed/">OurouiDev</a>
-        </div>
-      </div>
-`
+  `
 });
 
 
