d1ea690c42044f3666616fea68703800e2efe6c2
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2024-11-11 19:33:40 +0000

    Ctsgdxhjd chcdh

--- products/accounts/spotify/script.js
+++ products/accounts/spotify/script.js
@@ -64,9 +64,26 @@ fetch(ApiJson).then(Data =>{
                 DescTitle.innerHTML = "Description"
                 let Desc = document.createElement("p")
                 Desc.innerHTML = `${Item.ItemDescription}`
+
+                let PayBtns = document.createElement("div")
+                PayBtns.className = "pay-btns"
+                let link1 = document.createElement('a')
+                link1.innerHTML = "Shopify"
+                link1.href = Item.ItemUrl1
+                link1.className = "shopify"
+                let link2 = document.createElement('a')
+                link2.innerHTML = "Paypal"
+                link2.href = Item.ItemUrl2
+                link2.className = "paypal"
+                let link3 = document.createElement('a')
+                link3.innerHTML = "More ways ?"
+                link3.href = Item.ItemUrl3
+                link3.className = "more"
+                PayBtns.append(link1 , link2 , link3)
+
                 let PopupText = document.createElement("div")
                 PopupText.className = "popup-text"
-                PopupText.append(popUpTitle ,PricePopUp , DescTitle ,Desc)
+                PopupText.append(popUpTitle ,PricePopUp , DescTitle ,Desc , PayBtns)
                 //Popup Image
                 let PopuPimg = document.createElement("img")
                 PopuPimg.src = `${src}/${Item.ItemImagePopUp}`
@@ -74,9 +91,13 @@ fetch(ApiJson).then(Data =>{
                 let closePopUp = document.createElement("ion-icon")
                 closePopUp.className = "close-popup"
                 closePopUp.name = "close-outline"
+
                 let PopuPContent = document.createElement("div")
                 PopuPContent.className = "popUp-content"
                 PopuPContent.append(closePopUp , PopuPimg ,PopupText)
+
+
+                
                 let PopUp = document.createElement("div")
                 PopUp.className = "item-popup"
                 PopUp.append(PopuPContent)
